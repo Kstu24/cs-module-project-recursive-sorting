08116dae6c8e36540c362046e93e0f9f6d7cd282
Kevin Stewart <kistewart419@gmail.com>
2020-10-02 23:09:39 -0400

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,6 +2,24 @@
 def binary_search(arr, target, start, end):
     # Your code here
 
+    if len(arr) == 0:
+        return -1
+    
+    center = (start + end) // 2
+
+    if start == end:
+        return -1
+    
+    if target == arr[center]:
+        return center
+    
+    if target > arr[center]:
+        start = center
+        return binary_search(arr, target, start, end)
+    
+    if target < arr[center]:
+        end = center
+        return binary_search(arr, target, start, end)
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +29,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -5,13 +5,38 @@ def merge(arrA, arrB):
 
     # Your code here
 
+    # pointers for index of both arrays
+    a_array_i = 0
+    b_array_i = 0
+
+    for i in range(elements):
+
+        if a_array_i < len(arrA) and b_array_i < len(arrB):
+            if arrA[a_array_i] <= arrB[b_array_i]:
+                merged_arr[i] = arrA[a_array_i]
+                a_array_i += 1
+            else:
+                merged_arr[i] = arrB[b_array_i]
+                b_array_i += 1
+        elif a_array_i < len(arrA) and b_array_i is len(arrB):
+            merged_arr[i] = arrA[a_array_i]
+            a_array_i += 1
+        else:
+            merged_arr[i] = arrB[b_array_i]
+            b_array_i += 1
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
-
+    if len(arr) > 1:
+        center = len(arr) // 2
+        left = arr[:center]
+        right = arr[center:]
+        left = merge_sort(left)
+        right = merge_sort(right)
+        return merge(left, right)
 
     return arr
 
@@ -21,8 +46,10 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    pass
 
